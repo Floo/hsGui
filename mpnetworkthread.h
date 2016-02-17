31740e1f6585ed729cc010d14fbef255c4166651
Florian <flo.fp@gmx.de>
2016-02-17 20:41:35 +0100

    Kompatibilität zu Qt4 hergestellt

--- mpnetworkthread.h
+++ mpnetworkthread.h
@@ -8,6 +8,14 @@
 #include <QThread>
 #include <QtNetwork/QTcpSocket>
 
+#define HAS_QT47 ( QT_VERSION >= 0x040700 )
+#define HAS_QT5  ( QT_VERSION >= 0x050000 )
+
+/* Q_DECL_OVERRIDE is a Qt5 feature, add empty define to not break with Qt4 */
+#if !HAS_QT5 && !defined(Q_DECL_OVERRIDE)
+#define Q_DECL_OVERRIDE
+#endif
+
 
 class MpNetworkThread : public QThread
 {
